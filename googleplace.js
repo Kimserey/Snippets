c85da8303325037316c26eec33f6904ae4106dbe
Kimserey <kimserey.lam@gmail.com>
2014-07-22 15:43:32 +0800

    added google place wrapper

---
+++ googleplace.js
@@ -0,0 +1,50 @@
+/*
+ * google place wrapper for dropdown
+*/
+
+/*jslint         browser: true, continue : true,
+  devel  : true, indent : 2,    maxerr   : 50,
+  newcap : true, nomen  : true, plusplus : true,
+  regexp : true, sloppy : true, vars     : false,
+  white  : true
+*/
+
+/*global google*/
+
+var googleplace = (function () {
+    var initialize, get_autocomplete, autocomplete, add_event_listener;
+
+    initialize = function (configMap) {
+        var input = configMap.input,
+            $lat = configMap.$lat,
+            $lng = configMap.$lng;
+
+        autocomplete = new google.maps.places.Autocomplete(input);
+
+        google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            var place = autocomplete.getPlace(),
+				lat = place.geometry.location.lat(),
+				lng = place.geometry.location.lng();
+            if ($lat) {
+                $lat.val(lat);
+            }
+            if ($lng) {
+                $lng.val(lng);
+            }
+        });
+    };
+
+    add_event_listener = function (event_name, callback) {
+        google.maps.event.addListener(autocomplete, event_name, callback);
+    };
+
+    get_autocomplete = function () {
+        return autocomplete;
+    };
+
+    return {
+        initialize: initialize,
+        autocomplete : get_autocomplete,
+        addEventListener: add_event_listener
+    };
+}());
