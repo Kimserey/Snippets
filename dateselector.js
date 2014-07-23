7d5dd6d7c14bd721e2f4d6b84fedf6808333691d
Kimserey <Kimserey.lam@gmail.com>
2014-07-23 23:02:49 +0800

    date selector jquery usage example

---
+++ dateselector.js
@@ -0,0 +1,82 @@
+/*
+* date selector
+*/
+
+/*jslint         browser   : true, continue : true,
+  devel  : true, indent : 2,    maxerr   : 50,
+  newcap : true, nomen  : true, plusplus : true,
+  regexp : true, sloppy : true, vars     : false,
+  white  : true
+*/
+
+/*global jQuery*/
+"use strict";
+
+var dateselector = (function ($) {
+    var init, setJqueryMap, 
+    	setDate, removeDate,
+        hideAndShow, jqueryMap;
+    
+    init = function () {
+        setJqueryMap();
+        hideAndShow();
+
+        //init datepicker
+        jqueryMap.datepicker.datepicker();
+
+        //init jquery event handler
+        jqueryMap.$date_btn.on("click", function() { hideAndShow(jqueryMap.$date_div); });
+        jqueryMap.$other_btn.on("click", function() { hideAndShow(jqueryMap.$other_div); });
+        jqueryMap.$hello_btn.on("click", function() { hideAndShow(jqueryMap.$hello_div); });
+        jqueryMap.$date_input_btn.on("click", setDate);
+        jqueryMap.$selected_date_remove.on("click", removeDate);
+    };
+
+    setJqueryMap = function () {
+        jqueryMap = {
+        	$datepicker : $("#datepicker"),
+        	$date_btn   : $(".select-date-btn"),
+			$other_btn  : $(".select-other-btn"),
+			$hello_btn  : $(".select-hello-btn"),
+			$date_input_btn : $(".select-date-input-btn"),
+			$selected_date_remove : $(".selected-date-remove"),
+
+			$date_div  : $(".select-date-div"),
+			$other_div : $(".select-other-div"),
+			$hello_div : $(".select-hello-div"),
+			$selected_date_div : $(".selected-date-div"),
+
+			$date_input : $(".select-date-input"),
+			$selected_date_span : $(".selected-date-span")
+        };
+    };
+    
+    setDate = function () {
+        var date = jqueryMap.$date_input.val();
+        if (!date) {
+            alert("No date selected");
+        }
+
+        jqueryMap.$selected_date_span.text(date);
+        jqueryMap.$selected_date_div.show();
+    };
+
+    removeDate = function () {
+    	jqueryMap.$selected_date_span.empty();
+    	jqueryMap.$selected_date_div.hide();
+    };
+
+    hideAndShow = function ($container) {
+        jqueryMap.$date_div.hide();
+        jqueryMap.$other_div.hide();
+        jqueryMap.$hello_div.hide();
+        
+        if ($container) {
+        	$container.show();
+    	}
+    };
+    
+    return {
+        init : init
+    };
+}(jQuery));
